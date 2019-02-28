22b90203d63ee648608730c658cae70254f86d77
Jose <jrsimog@gmail.com>
2019-02-28 05:31:02 -0300

    testing && instructions

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# IceScreenBuy
+# IceScreamBuy
+
 Felipe y Sebastian  tiene m pesos, los cuales quieren gastar en helados. El heladero ofrece N Sabores, y ellos quieren seleccionar dos distintos de manera que hagan uso de todo su dinero.
 
 Dado m y un arreglo de largo N, donde cada elemento corresponde a un sabor de helados y su valor es el precio de este, obtenga los indices de los elementos escogidos tal que la suma de sus valores sea igual a m.
@@ -13,4 +14,12 @@ Ejemplo de salida
 
 ```
 Salida [0,3]
+```
+# Ejecutar
+```
+npm start
+```
+# Ejecutar prueba unitaria
+```
+npm test
 ```
--- test/solution.spec.js
+++ test/solution.spec.js
@@ -1,9 +1,9 @@
 import {expect} from "chai";
 import {N, m, output_spec} from "../constants";
 import {getItemsToBuy} from "../src/solution";
-describe("Testing of assert function: ", ()=>{
+describe("Testing of expect function: ", ()=>{
     describe("Testing add function", ()=>{
-        it("Check returned value from getItemsToBuy(N,m), using output_spec[index]).to.equal(result[index])",()=>{
+        it("Check returned value from getItemsToBuy(N,m), using  expect(output_spec[index]).to.equal(result[index])",()=>{
             const result = getItemsToBuy(N,m);
             expect(output_spec[0]).to.equal(result[0])
             expect(output_spec[1]).to.equal(result[1])
